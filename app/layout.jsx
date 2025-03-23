0e339ff7e534eae656160536898137a16fce1b3c
Koji Tanaka <tenkoma@gmail.com>
2025-03-23 20:27:37 +0900

    feat: create `/posts`, `/tags/:id`, `/rss.xml` pages https://nextra.site/docs/blog-theme/posts

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,42 +4,25 @@ import { getPageMap } from 'nextra/page-map'
 import 'nextra-theme-blog/style.css'
  
 export const metadata = {
-  title: 'Blog Example'
+  title: 'tenkoma\'s blog'
 }
  
 export default async function RootLayout({ children }) {
-  const banner = (
-    <Banner storageKey="4.0-release">
-      🎉 Nextra 4.0 is released.{' '}
-      <a href="#" className="x:text-primary-600">
-        Read more →
-      </a>
-    </Banner>
-  )
- 
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="ja" suppressHydrationWarning>
       <Head backgroundColor={{ dark: '#0f172a', light: '#fefce8' }} />
       <body>
-        <Layout banner={banner}>
+        <Layout>
           <Navbar pageMap={await getPageMap()}>
-            <Search />
+            {/* <Search /> */}
             <ThemeSwitch />
           </Navbar>
  
           {children}
  
           <Footer>
-            <abbr
-              title="This site and all its content are licensed under a Creative Commons Attribution-NonCommercial 4.0 International License."
-              style={{ cursor: 'help' }}
-            >
-              CC BY-NC 4.0
-            </abbr>{' '}
-            {new Date().getFullYear()} © Dimitri POSTOLOV.
-            <a href="/feed.xml" style={{ float: 'right' }}>
-              RSS
-            </a>
+            {new Date().getFullYear()} © tenkoma
+            <a href="/feed.xml" style={{ float: 'right' }}>RSS</a>
           </Footer>
         </Layout>
       </body>
