d9fa0c790fa44b768cf6f02899e65cc49a4481a9
Koji Tanaka <tenkoma@gmail.com>
2025-03-26 00:02:01 +0900

    feat: year/month 形式のディレクトリ構成に

--- app/posts/get-posts.js
+++ app/posts/get-posts.js
@@ -1,16 +1,29 @@
 import { normalizePages } from 'nextra/normalize-pages'
 import { getPageMap } from 'nextra/page-map'
+import fs from 'fs'
+import path from 'path'
 
 async function getAllPosts() {
+  // 2階層目までを列挙する
+  const baseDir = path.join(process.cwd(), 'content/posts')
+  const postDirectories = fs.readdirSync(baseDir)
+    .filter(dir => fs.statSync(path.join(baseDir, dir)).isDirectory())
+    .flatMap(dir => {
+      const subDir = path.join(baseDir, dir)
+      return fs.readdirSync(subDir)
+        .filter(sub => fs.statSync(path.join(subDir, sub)).isDirectory())
+        .map(sub => path.join(dir, sub))
+    })
+
   let posts = []
-  const years = ['2025']
-  for (const year of years) {
+  for (const dir of postDirectories) {
     const { directories } = normalizePages({
-      list: await getPageMap(`/posts/${year}`),
-      route: `/posts/${year}`
+      list: await getPageMap(`/posts/${dir}`),
+      route: `/posts/${dir}`
     })
     posts = posts.concat(directories)
   }
+  console.log(posts)
   return posts
 }
  
