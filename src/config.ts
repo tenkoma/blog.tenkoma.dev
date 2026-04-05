333327ad0fcb0c1cfe9a8a8415038661dbc32b00
Koji Tanaka <tenkoma@gmail.com>
2026-04-06 01:17:54 +0900

    clean: トップページの見出し消す

--- src/config.ts
+++ src/config.ts
@@ -25,7 +25,7 @@ export const SITE_FAVICON = "🤧";
 export const NAME = "tenkoma";
 
 // will be used in the footer as the license of the content (e.g. "All right reserved" or "CC-BY-SA 4.0")
-export const LICENSE = "All right reserved.";
+export const LICENSE = "";
 
 // will be used to identify your bluesky account, so that likes and comments can be shown on your posts
 export const BLUESKY_IDENTIFIER = "tenkoma.net";
